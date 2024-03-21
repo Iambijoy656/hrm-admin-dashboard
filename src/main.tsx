d6caf08bd00d397baabbec847f9f09fabf9ac968
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2024-03-22 00:56:38 +0600

    sign up done

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,17 @@ import './css/style.css';
 import './css/satoshi.css';
 import 'jsvectormap/dist/css/jsvectormap.css';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Toaster } from 'react-hot-toast';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
+      <Toaster
+        position="top-center"
+      />
+
+
       <App />
     </Router>
   </React.StrictMode>,
--- src/pages/Authentication/SignUp.tsx
+++ src/pages/Authentication/SignUp.tsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import { useForm } from "react-hook-form"
 import { Link } from 'react-router-dom';
 import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
 import LogoDark from '../../images/logo/logo-dark.svg';
 import Logo from '../../images/logo/logo.svg';
 import DefaultLayout from '../../layout/DefaultLayout';
+import { PiIdentificationCardLight } from "react-icons/pi";
+import toast from 'react-hot-toast';
+import api from './../../Utilities/api';
 
 const SignUp: React.FC = () => {
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = async (data: any) => {
+    if (data.password !== data.confirmPassword) {
+      return toast.error("Password and Confirm password does not match");
+    }
+
+    const signupData = {
+      name: data.name,
+      email: data.email,
+      student_id: data.student_id,
+      password: data.password
+    }
+    console.log('signupData', signupData);
+    api
+      .post("/signup", signupData)
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          toast.success("SignUp complete Successfully");
+          reset()
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("Please try again");
+      });
+  }
+
+
+
   return (
     <DefaultLayout>
       <Breadcrumb pageName="Sign Up" />
@@ -15,12 +57,12 @@ const SignUp: React.FC = () => {
           <div className="hidden w-full xl:block xl:w-1/2">
             <div className="py-17.5 px-26 text-center">
               <Link className="mb-5.5 inline-block" to="/">
-                <img className="hidden dark:block" src={Logo} alt="Logo" />
-                <img className="dark:hidden" src={LogoDark} alt="Logo" />
+                <h1 className='text-3xl font-bold text-black dark:text-white '>Sign Up Our  Application</h1>
+                {/* <img className="hidden dark:block" src={Logo} alt="Logo" /> */}
+                {/* <img className="dark:hidden" src={LogoDark} alt="Logo" /> */}
               </Link>
               <p className="2xl:px-20">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                suspendisse.
+                Sing up and stay with us
               </p>
 
               <span className="mt-15 inline-block">
@@ -150,18 +192,21 @@ const SignUp: React.FC = () => {
 
           <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
             <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
-              <span className="mb-1.5 block font-medium">Start for free</span>
+              {/* <span className="mb-1.5 block font-medium">Start for free</span> */}
               <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
-                Sign Up to TailAdmin
+                Sign Up
               </h2>
 
-              <form>
+              <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
                   <label className="mb-2.5 block font-medium text-black dark:text-white">
                     Name
                   </label>
                   <div className="relative">
                     <input
+                      {...register("name", {
+                        required: "Name is required", // Provide a message for the required rule
+                      })}
                       type="text"
                       placeholder="Enter your full name"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
@@ -176,20 +221,15 @@ const SignUp: React.FC = () => {
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
                       >
-                        <g opacity="0.5">
-                          <path
-                            d="M11.0008 9.52185C13.5445 9.52185 15.607 7.5281 15.607 5.0531C15.607 2.5781 13.5445 0.584351 11.0008 0.584351C8.45703 0.584351 6.39453 2.5781 6.39453 5.0531C6.39453 7.5281 8.45703 9.52185 11.0008 9.52185ZM11.0008 2.1656C12.6852 2.1656 14.0602 3.47185 14.0602 5.08748C14.0602 6.7031 12.6852 8.00935 11.0008 8.00935C9.31641 8.00935 7.94141 6.7031 7.94141 5.08748C7.94141 3.47185 9.31641 2.1656 11.0008 2.1656Z"
-                            fill=""
-                          />
-                          <path
-                            d="M13.2352 11.0687H8.76641C5.08828 11.0687 2.09766 14.0937 2.09766 17.7719V20.625C2.09766 21.0375 2.44141 21.4156 2.88828 21.4156C3.33516 21.4156 3.67891 21.0719 3.67891 20.625V17.7719C3.67891 14.9531 5.98203 12.6156 8.83516 12.6156H13.2695C16.0883 12.6156 18.4258 14.9187 18.4258 17.7719V20.625C18.4258 21.0375 18.7695 21.4156 19.2164 21.4156C19.6633 21.4156 20.007 21.0719 20.007 20.625V17.7719C19.9039 14.0937 16.9133 11.0687 13.2352 11.0687Z"
-                            fill=""
-                          />
-                        </g>
+                        {/* SVG icon */}
                       </svg>
                     </span>
                   </div>
+                  {errors.name && (
+                    <p className="text-red-500 text-xs mt-1">{errors.name.message as React.ReactNode}</p> // Explicitly cast to ReactNode
+                  )}
                 </div>
+
 
                 <div className="mb-4">
                   <label className="mb-2.5 block font-medium text-black dark:text-white">
@@ -197,6 +237,9 @@ const SignUp: React.FC = () => {
                   </label>
                   <div className="relative">
                     <input
+                      {...register("email", {
+                        required: "email is required",
+                      })}
                       type="email"
                       placeholder="Enter your email"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
@@ -220,6 +263,46 @@ const SignUp: React.FC = () => {
                       </svg>
                     </span>
                   </div>
+                  {errors.email && (
+                    <p className="text-red-500 text-xs mt-1">{errors.email.message as React.ReactNode}</p> // Explicitly cast to ReactNode
+                  )}
+                </div>
+
+                <div className="mb-4">
+                  <label className="mb-2.5 block font-medium text-black dark:text-white">
+                    Student ID
+                  </label>
+                  <div className="relative">
+                    <input
+                      {...register("student_id", {
+                        required: "Student ID is required", // Provide a message for the required rule
+                      })}
+                      type="text"
+                      placeholder="Enter your student ID"
+                      className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                    />
+                    <span className="absolute right-4 top-4">
+                      <PiIdentificationCardLight className="text-2xl font-bold text-slate-400 dark:text-slate-500" />
+                      {/* <svg
+                        className="fill-current"
+                      width="22"
+                      height="22"
+                      viewBox="0 0 22 22"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      >
+                      <g opacity="0.5">
+                        <path
+                          d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
+                          fill=""
+                        />
+                      </g>
+                    </svg> */}
+                    </span>
+                  </div>
+                  {errors.student_id && (
+                    <p className="text-red-500 text-xs mt-1">{errors.student_id.message as React.ReactNode}</p> // Explicitly cast to ReactNode
+                  )}
                 </div>
 
                 <div className="mb-4">
@@ -228,6 +311,23 @@ const SignUp: React.FC = () => {
                   </label>
                   <div className="relative">
                     <input
+                      {...register("password", {
+                        required: "Password is required",
+                        minLength: {
+                          value: 6,
+                          message: "Password should be at least six characters",
+                        },
+                        maxLength: {
+                          value: 16,
+                          message: "Maximum length exceeded",
+                        },
+                        pattern: {
+                          value:
+                            /(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}[\]|\\:;"'<>,.?/_₹]).*/,
+                          message:
+                            "Your password must contain at least one letter and one special character.",
+                        },
+                      })}
                       type="password"
                       placeholder="Enter your password"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
@@ -255,6 +355,9 @@ const SignUp: React.FC = () => {
                       </svg>
                     </span>
                   </div>
+                  {errors.password && (
+                    <p className="text-red-500 text-xs mt-1">{errors.password.message as React.ReactNode}</p> // Explicitly cast to ReactNode
+                  )}
                 </div>
 
                 <div className="mb-6">
@@ -263,6 +366,15 @@ const SignUp: React.FC = () => {
                   </label>
                   <div className="relative">
                     <input
+                      {...register("confirmPassword", {
+                        required: true,
+                        pattern: {
+                          value:
+                            /(?=.*[a-z])(?=.*[~`!@#$%^&*()--+={}[\]|\\:;"'<>,.?/_₹]).*/,
+                          message:
+                            "Your password must contain at least one letter and one special character.",
+                        },
+                      })}
                       type="password"
                       placeholder="Re-enter your password"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
@@ -290,6 +402,9 @@ const SignUp: React.FC = () => {
                       </svg>
                     </span>
                   </div>
+                  {errors.confirmPassword && (
+                    <p className="text-red-500 text-xs mt-1">{errors.confirmPassword.message as React.ReactNode}</p> // Explicitly cast to ReactNode
+                  )}
                 </div>
 
                 <div className="mb-5">
@@ -349,8 +464,8 @@ const SignUp: React.FC = () => {
             </div>
           </div>
         </div>
-      </div>
-    </DefaultLayout>
+      </div >
+    </DefaultLayout >
   );
 };
 
